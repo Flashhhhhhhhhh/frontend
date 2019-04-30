17b23365942c482d0960a1dc29f46bf2b7eb300b
Tanner Villarete <tvillarete@gmail.com>
2019-04-30 09:11:29 -0700

    Remove /build from gitignore

---
+++ build/precache-manifest.e06fd19821dbb29ff29f52689ddf3c04.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3ce86ad35e58ffb9777",
+    "url": "/static/js/main.14124ede.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94300d04d37d9a4fddff",
+    "url": "/static/js/2.bc45f828.chunk.js"
+  },
+  {
+    "revision": "c1ead601878160af83367056cda74560",
+    "url": "/index.html"
+  }
+];
